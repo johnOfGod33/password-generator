a03376fe2056adf61fc9b7a2d4407481dfa28544
jeandedieu <jeandedieusessou@gmail.com>
2024-06-20 22:59:00 +0000

    update generate password function

--- Scripts/generatePassword.js
+++ Scripts/generatePassword.js
@@ -67,27 +67,3 @@ const generatePassword = (
 
   return password;
 };
-
-const generateButton = document.querySelector(
-  ".passwordSettings_options_generateButton button"
-);
-
-generateButton.addEventListener("click", () => {
-  const passwordLength = document.querySelector(
-    ".passwordSettings_options_characterLength_input input"
-  ).value;
-  const upperIsChecked = document.querySelector("#uppercase_checkbox").checked;
-  const lowerIsChecked = document.querySelector("#lowercase_checkbox").checked;
-  const numberIsChecked = document.querySelector("#numbers_checkbox").checked;
-  const symbolIsChecked = document.querySelector("#symbols_checkbox").checked;
-  const passwordArea = document.querySelector(".passwordArea_password");
-  const password = generatePassword(
-    Number(passwordLength),
-    upperIsChecked,
-    lowerIsChecked,
-    numberIsChecked,
-    symbolIsChecked
-  );
-
-  passwordArea.textContent = password;
-});
