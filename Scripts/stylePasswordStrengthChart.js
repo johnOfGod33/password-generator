7745d237a3aeb9a96f9879cb2f2d971f58315446
jeandedieu <jeandedieusessou@gmail.com>
2024-06-20 13:24:32 +0000

    Start style password rating chart

---
+++ Scripts/stylePasswordStrengthChart.js
@@ -0,0 +1,24 @@
+const passwordStrengthChart = document.getElementsByClassName(
+  "passwordSettings_options_passwordStrength_info_chart_div"
+);
+
+const passwordStrengthName = document.querySelector(
+  ".passwordSettings_options_passwordStrength_info_name"
+);
+
+const strengthList = {
+  1: { name: "WEAK", color: "red" },
+  2: { name: "MEDIUM", color: "orange" },
+  3: { name: "STRONG", color: "green" },
+  4: { name: "VERY STRONG", color: "gold" },
+};
+
+const stylePasswordChart = (n) => {
+  passwordStrengthName.textContent = strengthList[n].name;
+
+  for (let i = 0; i < n; i++) {
+    passwordStrengthChart[i].style.backgroundColor = strengthList[n].color;
+  }
+};
+
+stylePasswordChart(3);
